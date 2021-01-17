3b1cca3a4304a4ed99882563ef33862c5dd3a966
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2021-01-17 16:14:59 +0100

    update

--- web/aufgabenvorschlaege.php
+++ web/aufgabenvorschlaege.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * User: Hans-Gert Gräbe
- * Last Update: 2021-01-16
+ * Last Update: 2021-01-17
  *
  */
 
@@ -19,24 +19,44 @@ function AufgabenVorschlaege() {
       $id=str_replace('https://www.mathematik-olympiaden.de/aufgaben/rdf/AufgabenVorschlag/','',$v->getURI());
       $gebiet=join(", ",$v->all('mo:zumGebiet'));
       $schwierigkeit=join(", ",$v->all('mo:hatSchwierigkeit'));
-      if ($v->get("owl:sameAs")) { $mo=$v->get("owl:sameAs")->get("mo:nr"); }
-      $a[]='<tr align="center"><td>'.$id.'</td> <td>'.$schwierigkeit.'</td> <td>'.$gebiet.'</td> <td>'
-          .$mo.'</td> </tr>';
+      $status=$v->get('mo:hatStatus');
+      if ($v->all("owl:sameAs")) {
+          $b=array();
+          foreach ($v->all("owl:sameAs") as $mo) {
+              if ($mo->get("mo:nr")) { $b[]=$mo->get("mo:nr"); }
+          }
+          $status=join(", ",$b);
+      }
+      $a[]='<tr align="center"><th scope="row">'.$id.'</th> <td>'
+          .$schwierigkeit.'</td> <td>'.$gebiet.'</td> <td>'
+          .$status.'</td> </tr>';
     }
     return '
 <div class="container">
-<table align="center" border="1"> 
-<tr align="center"> <th> Aufgabe </th> <th> Schwierigkeit </th> <th> Gebiet </th> <th> MO-Aufgabe </th>  </tr>'.
-    join("\n",$a).'</table></div>';
+<table class="table table-bordered table-hover"> 
+<thead>
+  <tr> 
+   <th scope="col" class="text-center"> Aufgabe </th> 
+   <th scope="col" class="text-center"> Schwierigkeit </th> 
+   <th scope="col" class="text-center"> Gebiet </th> 
+   <th scope="col" class="text-center"> Status </th>  
+  </tr>
+</thead>
+<tbody>'.
+    join("\n",$a).'</tbody></table></div>';
 }
 
 $content='
 <div class="container">
 <h2 align="center"> Klassifizierung von Aufgabenvorschlägen </h2>
 
-<p>Diese Übersicht wurde aus den Metadaten der Aufgaben der AAG 9/10
-extrahiert.</p>
-</div>
+<p>Hier nun eine etwas umfassendere Übersicht der Aufgaben der AAG 9/10, die
+aus den Metadaten der Aufgaben extrahiert wurde.  In der letzten Spalte ist
+angegeben, wann die Aufgabe gestellt wurde, wenn sie schon gestellt wurde,
+wobei hier nur Aufgaben aus Olympiaden gelistet werden, die schon
+veröffentlicht sind.  Aus technischen Gründen sind eine Reihe von Aufgaben die
+parallel in Klasse 9 und 10 gestellt wurden, unter "Status" nur einmal
+gelistet. </p> </div>
 ';
 
 echo showPage($content.AufgabenVorschlaege());
