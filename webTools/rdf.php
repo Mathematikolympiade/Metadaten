bd778d5237c141fb90abf3f9cb02c4b63e302963
Roger Labahn <roger.labahn@uni-rostock.de>
2022-06-03 15:01:30 +0200

    parseFile implemented

--- webTools/rdf.php
+++ webTools/rdf.php
@@ -5,9 +5,8 @@ use EasyRdf\Graph;
 require_once "lib/EasyRdf.php";
 
 $graph = new Graph();
+$graph->parseFile('../../mdRDF/models/math', 'text/turtle');
+$graph->parseFile('../../mdRDF/models/mo', 'text/turtle');
 $graph->parseFile('../../mdRDF/moGebiete.ttl', 'text/turtle');
-//$graph->parseFile('../web/rdf/MO-Basisdaten.rdf');
 
 echo $graph->dump();
-
-
