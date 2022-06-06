e32f6c9490b8886eb60175b81b9d3a8845c8a656
Roger Labahn <roger.labahn@uni-rostock.de>
2022-06-06 16:32:12 +0200

    DL example finished

--- webTools/rdf.php
+++ webTools/rdf.php
@@ -3,20 +3,22 @@
 require_once "lib/EasyRdf.php";
 require_once "thema.php";
 
-use EasyRdf\Graph, EasyRdf\RdfNamespace;
 
-RdfNamespace::set('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
-RdfNamespace::set('rdfs', 'http://www.w3.org/2000/01/rdf-schema#');
-RdfNamespace::set('math', 'https://www.mathematik-olympiaden.de/aufgaben/rdf/models/math#');
-RdfNamespace::set('mo', 'https://www.mathematik-olympiaden.de/aufgaben/rdf/models/mo#');
 
-$graph = new Graph();
-$graph->parseFile('../../mdRDF/models/math', 'text/turtle');
-$graph->parseFile('../../mdRDF/models/mo', 'text/turtle');
-$graph->parseFile('../../mdRDF/buchThemen.ttl', 'text/turtle');
+$writer = new ThemenHandler("../../mdRDF/");
+$writer->readTTL("buchThemen.ttl", 'text/turtle');
 
-echo $graph->countTriples() . " Triples successfully imported";
-//echo $graph->dump();
+echo $writer->graph->countTriples() . " Triples successfully imported";
+?>
 
-$resArray = $graph->allOfType("math:Thema");
-writeDL($resArray, RdfNamespace::expand("math:Gebiet"));
+<html lang="de">
+<header>
+    <link rel="stylesheet" type="text/css" href="rdf.css"/>
+    <script type="application/javascript">
+    </script>
+</header>
+<body>
+<h2>Thematisches Register</h2>
+<?php $writer->writeDL("math:Gebiet", 0); ?>
+</body>
+</html>
--- webTools/thema.php
+++ webTools/thema.php
@@ -1,15 +1,49 @@
 <?php
 
-use EasyRdf\RdfNamespace;
+use EasyRdf\Graph, EasyRdf\RdfNamespace;
 
-function writeDL($resArray, $thema) {
-    foreach ($resArray as $res) {
-//    echo $res->dump("text") ;
-        if ($res->get("rdfs:subClassOf")==$thema) {
-            echo "\n<dt>".$res->label()."</dt>";
-            echo "\n<dd><dl>";
-            writeDL($resArray, $res->getUri());
-            echo "\n</dl></dd>";
+RdfNamespace::set('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+RdfNamespace::set('rdfs', 'http://www.w3.org/2000/01/rdf-schema#');
+RdfNamespace::set('math', 'https://www.mathematik-olympiaden.de/aufgaben/rdf/models/math#');
+RdfNamespace::set('mo', 'https://www.mathematik-olympiaden.de/aufgaben/rdf/models/mo#');
+
+class ThemenHandler {
+    public $graph, $themen;
+		private $dataBaseDir;
+
+    function __construct($dataBaseDir) {
+				$this->dataBaseDir = $dataBaseDir;
+        $this->graph = new Graph();
+        $this->graph->parseFile($this->dataBaseDir.'models/math', 'text/turtle');
+        $this->graph->parseFile($this->dataBaseDir.'models/mo', 'text/turtle');
+        $this->updateThemen();
+    }
+
+    private function updateThemen() {
+        $this->themen = $this->graph->allOfType("math:Thema");
+    }
+
+    public function readTTL($fileName) {
+        $this->graph->parseFile($this->dataBaseDir.$fileName, 'text/turtle');
+        $this->updateThemen();
+    }
+
+    public function writeDL($thema, $level) {
+        $themen = array();
+        foreach ($this->themen as $thm) if ($thm->get("rdfs:subClassOf")==RdfNamespace::expand($thema)) {
+            $themen[] = $thm;
+        }
+        if (count($themen)>0) {
+            echo '<dl class="mds subject-level-'.$level.'">';
+            foreach ($themen as $thm) {
+                echo '<dt class="mds subject-level-'.$level.'">'.$thm->label().'</dt>';
+                echo '<dd class="mds subject-level-'.$level.'">';
+                $this->writeDL($thm->getUri(), $level+1);
+                echo '</dd>';
+            }
+            echo '</dl>';
         }
     }
+
 }
+
